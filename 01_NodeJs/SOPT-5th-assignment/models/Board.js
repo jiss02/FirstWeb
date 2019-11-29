67592067238aa96b5b45f6e0f925c6b58240b313
jiss02 <olo5112olo@naver.com>
2019-11-29 19:03:30 +0900

    board CRUD 추가

---
+++ 01_NodeJs/SOPT-5th-assignment/models/Board.js
@@ -0,0 +1,158 @@
+const pool = require('../modules/util/pool');
+const sc = require('../modules/util/statusCode');
+const rm = require('../modules/util/responseMessage');
+const util = require('../modules/util/util');
+
+const table = 'board';
+
+module.exports = {
+    read: (blogIdx) => {
+        const q = `SELECT * FROM ${table} WHERE blogIdx = ${blogIdx} `;
+        const sendData = pool.queryParam_None(q)
+        .then(result => {
+            // 존재하는지 판단
+            if(!result[0]){
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse(rm.NO_BOARD, sc.BAD_REQUEST)
+                }
+            }
+            return {
+                code: sc.OK,
+                json: util.successTrue(rm.BOARD_READ_ALL_SUCCESS, sc.OK, result)
+            }
+        })
+        .catch(err=>{
+            throw err;
+        });
+        return sendData;
+    },
+
+    readOne: (blogIdx, boardIdx) => {
+        const q = `SELECT * FROM ${table} WHERE blogIdx = ${blogIdx} and boardIdx = ${boardIdx}`;
+        const sendData = pool.queryParam_None(q)
+        .then(result => {
+            // 존재하는지 판단
+            if(!result[0]){
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse(rm.NO_BOARD, sc.BAD_REQUEST)
+                }
+            }
+            return {
+                code: sc.OK,
+                json: util.successTrue(rm.BOARD_READ_SUCCESS, sc.OK, result)
+            }
+        })
+        .catch(err=>{
+            throw err;
+        });
+        return sendData;
+    },
+
+    create: ({userIdx, blogIdx, title, content, imgs}) => {
+        const fields = '`userIdx`, `blogIdx` ,`title`, `content`, `imgs`,`created`, `updated`';
+        const questions = `?,?,?,?,?,?,?`;
+        const created = new Date().toLocaleString().slice(0,18);
+        const updated = new Date().toLocaleString().slice(0,18);
+        const values = [userIdx, blogIdx, title, content, imgs, created, updated];
+        const q = `INSERT INTO ${table}(${fields}) VALUES (${questions})`;
+        
+        const sendData = pool.queryParam_Parse(q, values)
+        .then(result=>{
+            console.log(result);
+            return {
+                code: sc.CREATED,
+                json: util.successTrue(rm.BOARD_CREATE_SUCCESS, sc.CREATED)
+            }
+        })
+        .catch(err => {
+            if(err.errno === 1452){
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse(rm.NO_BLOG, sc.BAD_REQUEST)
+                }
+            }
+            throw err;
+        });
+        return sendData;
+    },
+
+    update: async ({userIdx, blogIdx, boardIdx, title, content, imgs}) => {
+        const checkquery = `SELECT userIdx FROM ${table} WHERE blogIdx = ${blogIdx} and boardIdx = ${boardIdx}`
+        const sendData = pool.queryParam_None(checkquery)
+        .then(result => {
+            // 존재하는지 판단
+            if(!result[0]){
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse(rm.NO_BOARD, sc.BAD_REQUEST)
+                }
+            }
+            // 작성자인지 판단
+            if(result[0].userIdx !== userIdx) {
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse("생성자와 일치하지 않습니다.", sc.BAD_REQUEST)
+                }
+            }
+            // 작성자와 일치하는 경우
+            const updated = new Date().toLocaleString().slice(0,18);
+            const q = `UPDATE \`${table}\` SET title='${title}', content='${content}', imgs = '${imgs}', updated='${updated}' WHERE boardIdx=${boardIdx}`;
+            const updateData = pool.queryParam_None(q)
+            .then(result => {
+                return {
+                    code: sc.OK,
+                    json: util.successTrue(rm.BOARD_UPDATE_SUCCESS, sc.OK, result)
+                }
+            })
+            .catch(err=>{
+                if(err.errno === 1452){
+                    return {
+                        code: sc.BAD_REQUEST,
+                        json: util.successFalse(rm.NO_BLOG, sc.BAD_REQUEST)
+                    }
+                }
+                throw err;
+            });
+            return updateData;
+        });
+        return sendData;
+    },
+
+    delete: (userIdx, blogIdx, boardIdx) => {
+        const checkquery = `SELECT userIdx FROM ${table} WHERE blogIdx = ${blogIdx} and boardIdx = ${boardIdx}`
+        const sendData = pool.queryParam_None(checkquery)
+        .then(result => {
+            // 존재하는지 판단
+            if(!result[0]){
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse(rm.NO_BOARD, sc.BAD_REQUEST)
+                }
+            }
+            // 작성자인지 판단
+            if(result[0].userIdx !== userIdx) {
+                return {
+                    code: sc.BAD_REQUEST,
+                    json: util.successFalse("생성자와 일치하지 않습니다.", sc.BAD_REQUEST)
+                }
+            }
+            // 작성자와 일치하는 경우
+            const q = `DELETE FROM ${table} WHERE boardIdx = ${boardIdx}`;
+            const updateData = pool.queryParam_None(q)
+            .then(result => {
+                return {
+                    code: sc.OK,
+                    json: util.successTrue(rm.BOARD_DELETE_SUCCESS, sc.OK, result)
+                }
+            })
+            .catch(err=>{
+                throw err;
+            });
+            return updateData;
+        });
+        return sendData;
+    },
+    
+}
--- 01_NodeJs/SOPT-5th-assignment/routes/blog/board/board.js
+++ 01_NodeJs/SOPT-5th-assignment/routes/blog/board/board.js
@@ -1,26 +1,84 @@
 var express = require('express');
 var router = express.Router({ mergeParams: true });
+const util = require('../../../modules/util/util');
+const sc = require('../../../modules/util/statusCode');
+const rm = require('../../../modules/util/responseMessage');
+const check = require('../../../modules/util/nullcheck');
+const Board = require('../../../models/Board');
 
-router.get('/', (req, res)=> {
-
+router.get('/', (req, res)=>{
+    const blogIdx = req.params.blogIdx;
+    Board.read(blogIdx)
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(util.successFalse(rm.INTERNAL_SERVER_ERROR, sc.INTERNAL_SERVER_ERROR));
+    })
 });
 
-router.get('/:boradIdx', (req, res)=> {
-
+router.get('/:boardIdx', (req, res)=>{
+    const {blogIdx, boardIdx} = req.params;
+    Board.readOne(blogIdx, boardIdx)
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(util.successFalse(rm.INTERNAL_SERVER_ERROR, sc.INTERNAL_SERVER_ERROR));
+    });
 });
 
-
-router.use('/', require("../../modules/util/authUtil").LoggedIn);
+router.use('/', require("../../../modules/util/authUtil").LoggedIn);
 router.post('/', (req, res)=> {
-
+    const decoded = req.decoded;
+    const blogIdx = req.params.blogIdx;
+    const {title, content, imgs} = req.body;
+    if(!title || !content || !imgs){
+        const miss = check.isnull({title, content, imgs});
+        res.status(sc.BAD_REQUEST)
+        .send(util.successFalse(`${rm.NULL_VALUE} 없는 값은 ${miss} 입니다`, sc.BAD_REQUEST));
+    }
+    Board.create({decoded, blogIdx ,title, content, imgs})
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(util.successFalse(rm.INTERNAL_SERVER_ERROR, sc.INTERNAL_SERVER_ERROR));
+    })
 });
 
 router.put('/:boardIdx', (req, res)=> {
-
+    const decoded = req.decoded;
+    const {blogIdx, boardIdx} = req.params;
+    const {title, content, imgs} = req.body;
+    if(!title || !content || !imgs){
+        const miss = check.isnull({title, content, imgs});
+        res.status(sc.BAD_REQUEST)
+        .send(util.successFalse(`${rm.NULL_VALUE} 없는 값은 ${miss} 입니다`, sc.BAD_REQUEST));
+    }
+    Board.update({decoded, blogIdx, boardIdx, title, content, imgs})
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(util.successFalse(rm.INTERNAL_SERVER_ERROR,sc.INTERNAL_SERVER_ERROR));
+    });
 });
 
 router.delete('/:boardIdx', (req, res)=>{
-
+    const decoded = req.decoded;
+    const {blogIdx, boardIdx} = req.params;
+    Board.delete(decoded, blogIdx, boardIdx)
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(util.successFalse(rm.INTERNAL_SERVER_ERROR,sc.INTERNAL_SERVER_ERROR));
+    });
 });
-
 module.exports = router;
